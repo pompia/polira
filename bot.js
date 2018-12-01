8daf18c3bb48d7beb4a627182ec14ba5091baf57
pompia <45210867+pompia@users.noreply.github.com>
2018-12-01 23:11:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+var prefix = "*";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+
+
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+
+
+      });
 
 client.on('ready', () => {
    console.log(`----------------`);
